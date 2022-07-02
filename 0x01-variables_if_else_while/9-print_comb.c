147d85f75daa340074b3f31b61a0accdd347c721
Bernard Owoo <smilebennygatho@gmail.com>
2022-07-01 17:06:16 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stido.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entery point
+ *
+ * Renturn: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+		if (d != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
