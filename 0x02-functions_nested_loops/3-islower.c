c235afea8e2c7ab5df1402284ae50bdba7440f91
Bernard Owoo <smilebennygatho@gmail.com>
2022-07-05 19:47:47 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,6 +5,7 @@
  * @c: An input character
  * Description: fuction uses _putchar function to print
  * alphabet in lowercase or if is uppercase
+ * Return: 1 if is lowercase or 0 if is uppercase
  */
 int _islower(int c)
 {
