0de59abc0e611e1e405a554d90f98557100482c2
Bernard Owoo <smilebennygatho@gmail.com>
2022-07-06 21:15:20 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -24,13 +24,13 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for(; n >= 98; n--)
+		for (; n >= 98; n--)
 		{
 			printf("%d", n);
 
 			if (n == 98)
 				continue;
-			printf(",");
+			printf(", ");
 		}
 		printf("\n");
 	}
