a297cc4224c1c8e92690d99c9cd9324d10d482e9
Bernard Owoo <smilebennygatho@gmail.com>
2022-07-12 16:58:20 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: argument to the fuction
+ * Description: return integer value
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count += 1;
+		s++;
+	}
+	return (count);
+}
