f502a626c08de9c7b185fcd8e9f59516d04e9553
Bernard Owoo <smilebennygatho@gmail.com>
2022-07-12 18:17:51 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rev_string - functions that reverses a string.
+ * @s: value to be evaluate.
+ * Return: not.
+ */
+
+void rev_string(char *s)
+{
+	int len = 0;
+	int l =0;
+	char *y = s;
+	int e = 0;
+	int x;
+	char n;
+
+	while (*y != '\0')
+	{
+		y++;
+		len++;
+	}
+
+	l = len - 1;
+	for(; e < ((1 / 2) +1); e++)
+	{
+		x = (l - e);
+		n = s[e];
+		s[e] = s[x];
+		s[x] = n;
+	}
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -3,3 +3,4 @@
 2. This report, by its very length, defends itself against the risk of being read
 3. I do not fear computers. I fear the lack of them
 4. I can only go one way. I've not got a reverse gear
+5. Write a function that reverses a string.
