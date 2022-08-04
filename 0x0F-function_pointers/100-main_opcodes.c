c8b0820a872c277b1cc5026f96d0f1396afc8340
Bernard Owoo <smilebennygatho@gmail.com>
2022-08-03 20:47:31 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,34 +2,44 @@
 #include <stdlib.h>
 
 /**
- * main - prints opcode of own main function
- * @argc: argument count
- * @argv: array of arguments
- * Return: 1 or 2 on fail, 0 on success
- */
+*main -  program that prints the opcodes of its own main function.
+*@argc: number of arguments passed to the program
+*@argv: array of arguments
+*
+*Return: on success, 1 or 2 in case of failure
+*/
+
 int main(int argc, char *argv[])
 {
-	int bytes, i;
-	unsigned char *func_ptr;
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
-	bytes = atoi(argv[1]);
-	if (bytes < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-	func_ptr = (unsigned char *)main;
-	i = 0;
-	if (bytes > 0)
-	{
-		while (i < (bytes - 1))
-			printf("%02hhx ", func_ptr[i++]);
-		printf("%hhx\n", func_ptr[i]);
-	}
-	return (0);
+int index, bytes;
+int (*address)(int, char **) = main;
+unsigned char opcode;
+
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+
+bytes = atoi(argv[1]);
+
+if (bytes < 0)
+{
+printf("Error\n");
+exit(2);
+}
+
+for (index = 0; index <  bytes; index++)
+{
+opcode = *(unsigned char *)address;
+printf("%.2x", opcode);
+
+if (index == bytes - 1)
+continue;
+printf(" ");
+address++;
+}
+printf("\n");
+return (0);
 }
